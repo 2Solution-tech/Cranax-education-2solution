edc82e644be171d45205eb8c6a445082f757a38a
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-05-16 20:55:43 +0530

    Added met tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import { Navbar } from "@/component/Navbar";
 import Footer from "@/component/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +15,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <Navbar/>
-        {children}
-        <Footer/>
-        </body>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="google-site-verification" content="MixRzyYRWjSq8iva2JYqMcGU4TcFL4sNF4thklbWz8k" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y5Y22LYQJ5"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y5Y22LYQJ5');
+            `,
+          }}
+        ></script>
+
+      </Head>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
